1547ff6334fa56eba4fb141f362553a8467d799e
Rodrigo Antunes <rodrigoantunes@Admins-MBP.lan>
2020-04-16 16:32:01 +0100

    map json on javascript code

--- src/index.js
+++ src/index.js
@@ -1,32 +1,44 @@
 import Phaser from "phaser";
 import logoImg from "./assets/logo.png";
+import mapPNG from "./assets/assetsmap.png";
+import mapJSON from "./assets/map.json";
+import water from "./assets/water.png";
 
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
-  width: 800,
-  height: 600,
+  width: 650,
+  height: 650,
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   }
 };
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("logo", logoImg);
+
+  this.load.image("background", water);
+  
+
+  this.load.image("tiles", mapPNG);
+  this.load.tilemapTiledJSON("map", mapJSON);
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
+  const map = this.make.tilemap({key : "map"});
+  const tileset = map.addTilesetImage("assets", "tiles");
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+  this.add.image(650, 650, "background");
+  
+  const ground = map.createStaticLayer("ground", tileset, 0, 0);
+  const objectCollider = map.createStaticLayer("objectCollider", tileset, 0, 0);
+  const aboveCollider = map.createStaticLayer("aboveObject", tileset, 0, 0);
+
+}
+
+function update(){
+  
 }
